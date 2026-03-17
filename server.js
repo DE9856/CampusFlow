9195a56a8a52a9bbecd3e10d16d7eaaf0bb6f6ef
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-03-17 15:38:34 +0530

    Updated server.py

--- server.js
+++ server.js
@@ -274,6 +274,8 @@ app.post("/send", async (req, res) => {
 // ============================
 // ▶️ START SERVER
 // ============================
-app.listen(5000, () => {
-  console.log("🚀 Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
